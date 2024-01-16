6875a15f49deef61ba399968fa406770be3c1417
AmanK <amankumar.m99@gmail.com>
2024-01-16 18:38:51 +0530

    added view TS of cards

--- src/app/components/view-card/view-card.component.ts
+++ src/app/components/view-card/view-card.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { AccountById } from 'src/app/models/account/account-by-id';
+import { Card } from 'src/app/models/card/card';
+import { CardById } from 'src/app/models/card/card-by-id';
+import { CardService } from 'src/app/services/card/card.service';
 
 @Component({
   selector: 'app-view-card',
@@ -6,5 +12,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./view-card.component.css']
 })
 export class ViewCardComponent {
+  cardId:string;
+  editCardForm:FormGroup;
+  modalTitle = '';
+  modalTitleStyleClass = '';
+  modalBody = '';
+  modalCloseBtnStyleClass = '';
+  submitTextSuffix = '';
+  disableSubmitBtn = false;
+  card:Card|null;
 
+  constructor(
+    private activatedRoute:ActivatedRoute,
+    private formBuilder:FormBuilder,
+    private cardService:CardService
+  ){
+    this.cardId = this.activatedRoute.snapshot.params['cardId'];
+    this.card=null;
+    this.editCardForm = this.formBuilder.group({});
+    this.cardService.getCardById(new CardById(this.cardId)).subscribe(response=>{
+      this.card = response;
+      this.editCardForm = this.formBuilder.group({})
+      });
+  }
 }
